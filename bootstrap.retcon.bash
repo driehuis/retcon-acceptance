24725136909aea09cc8100781991db5b6583b11b
Bert Driehuis <bert.driehuis@cyso.nl>
2015-05-02 13:01:09 +0200

    Get webserver up and running

--- bootstrap.retcon.bash
+++ bootstrap.retcon.bash
@@ -1,8 +1,11 @@
 set -e
-#sudo apt-get update -qq && sudo apt-get dist-upgrade
+sudo rsync -rl /vagrant/files/./ /./
+sudo sh -c "echo '# Cleared by $0, using sources.list.d instead' >/etc/apt/sources.list"
+wget http://plank.cyso.net/linux/apt.cyso.net.pub.key -q -O - | sudo apt-key add -
+sudo apt-get update -qq && sudo apt-get dist-upgrade
 sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev\
  zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev git libsqlite3-dev subversion libpq-dev\
- nginx
+ nginx cyso-firewall
 [ -f $HOME/.gemrc ] || echo gem: --no-ri --no-rdoc >$HOME/.gemrc
 
 cd $HOME
@@ -15,12 +18,13 @@ if [ ! -d .rbenv ]; then
   git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
   git clone git://github.com/carsomyr/rbenv-bundler.git ~/.rbenv/plugins/bundler
-  if ! grep -q rbenv .profile; then
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
-    echo 'eval "$(rbenv init -)"' >> ~/.profile
-  fi
   . ~/.profile
   bash -l -c 'rbenv install `cat $HOME/retcon-web/.ruby-version`'
+fi
+
+if ! grep -q rbenv .profile; then
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+  echo 'eval "$(rbenv init -)"' >> ~/.profile
 fi
 
 bash -l -c 'cd $HOME/retcon-web; which bundle || gem install -v=1.7.6 bundler'
@@ -28,7 +32,6 @@ bash -l -c 'cd $HOME/retcon-web; which passenger || gem install -v=4.0.59 passen
 if [ ! -e retcon-web/config/database.yml ]; then
   cp -i retcon-web/config/database.yml.sample retcon-web/config/database.yml
 fi
-sudo rsync -ra /vagrant/files/./ /./
 sudo service nginx restart
 if [ ! -e retcon-web/config/.secret ]; then
   ps axuww | md5sum - | cut -b1-32 > retcon-web/config/.secret
--- seed_acceptance.rb
+++ seed_acceptance.rb
@@ -1,1 +1,7 @@
-u=User.create(:username=>"admin", :password=>"Repelst33ltje")
+u=User.find_or_create_by_username("retcon-acc")
+u.password_confirmation = u.password = "Repelst33ltje"
+u.save
+r1=Role.find_or_create_by_name("admin")
+r2=Role.find_or_create_by_name("user")
+r3=Role.find_or_create_by_name("agent")
+ru=RolesUser.find_or_create_by_role_id_and_user_id(r1.id, u.id)
